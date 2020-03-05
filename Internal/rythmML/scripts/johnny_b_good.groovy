194d60fca4c7d3b015551abdfb3edc0a3fd67cd4
Rostaing-Enzo <enzo.rostaing@etu.unice.fr>
2020-03-05 22:14:20 +0100

    Update johnny_b_good.groovy

--- Internal/rythmML/scripts/johnny_b_good.groovy
+++ Internal/rythmML/scripts/johnny_b_good.groovy
@@ -1,25 +1,21 @@
-song "Johnny Be Good" bpm 170
+song "Johnny Be Good" bpm 168
 
 sequence "part1" bars 1 beats 4
 
 
 
 
-play "si" sequence "part1" bar 1 beats 1 of 5
-play "si" sequence "part1" bar 1 beats 2 of 5
-play "si" sequence "part1" bar 1 beats 3 of 5
-play "si" sequence "part1" bar 1 beats 4 of 5
-play "do" sequence "part1" bar 1 beats 1 of 5
-play "do" sequence "part1" bar 1 beats 3 of 5
+play "rc" sequence "part1" bar 1 beats 1 of 5
+play "rc" sequence "part1" bar 1 beats 2 of 5
+play "rc" sequence "part1" bar 1 beats 3 of 5
+play "rc" sequence "part1" bar 1 beats 4 of 5
+play "bd" sequence "part1" bar 1 beats 1 of 5
+play "bd" sequence "part1" bar 1 beats 3 of 5
 play "sd" sequence "part1" bar 1 beats 2 of 5
 play "sd" sequence "part1" bar 1 beats 4 of 5
 
-
-
-
-
 track "Drums"  instrument "drum"
 
-assign 4 times "part1" to  "Drums"
+assign 34 times "part1" to  "Drums"
 
 export "Switch!"
